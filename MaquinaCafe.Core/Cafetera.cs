638bb779b11044cce4be6dbd22cd481937778777
Eduardo Rojas <edurojas10.28877@gmail.com>
2026-03-13 21:18:08 -0400

    Se agregaron validaciones a la clase Cafetera para evitar valores negativos y que no se pueda dar una cantidad negativa de cafe

--- MaquinaCafe.Core/Cafetera.cs
+++ MaquinaCafe.Core/Cafetera.cs
@@ -6,6 +6,8 @@
 
         public Cafetera(int cantidadCafe)
         {
+            if (cantidadCafe < 0)
+                throw new ArgumentOutOfRangeException(nameof(cantidadCafe), "La cantidad inicial no puede ser negativa.");
             _cantidadCafe = cantidadCafe;
         }
 
@@ -16,20 +18,28 @@
 
         public void SetCantidadCafe(int cantidad)
         {
+            if (cantidad < 0)
+                throw new ArgumentOutOfRangeException(nameof(cantidad), "La cantidad no puede ser negativa.");
             _cantidadCafe = cantidad;
         }
 
         public bool HasCafe(int cantidad)
         {
+            if (cantidad <= 0)
+                throw new ArgumentOutOfRangeException(nameof(cantidad), "La cantidad a consultar no puede ser negativa o cero.");
             return _cantidadCafe >= cantidad;
         }
 
         public void GiveCafe(int cantidad)
         {
-            if (HasCafe(cantidad))
-            {
-                _cantidadCafe -= cantidad;
-            }
+            if (cantidad < 0)
+                throw new ArgumentOutOfRangeException(nameof(cantidad), "No se puede dar una cantidad negativa de café.");
+
+            
+            if (!HasCafe(cantidad))
+                throw new InvalidOperationException("No hay suficiente café en la máquina para despachar esta cantidad.");
+
+            _cantidadCafe -= cantidad;
         }
     }
 }
